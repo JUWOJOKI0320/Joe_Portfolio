50f802fc2636fcde92a6eace178eee27ae9ad87d
JUWOJOKI0320 <joechristianlina0320@gmail.com>
2025-01-29 13:31:35 +0800

    fixed the sidemenu again

--- script.js
+++ script.js
@@ -43,7 +43,6 @@ menu.addEventListener("click", () => {
 
 sidemenuButton.addEventListener("click", () =>{
     sideMenu.classList.toggle("active");
-    sidemenuButton.style.transform = "rotate(90deg)";
 });
 
 function handleModeChange(event) {
